68d95836b13fb7dc6329d2989c818ce25951d5c8
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2025-08-18 16:54:02 -0400

    Create colors.lua
    i dont use this one, if you wanted to youd just have to call the funciton

---
+++ ~.config/nvim/after/plugin/colors.lua
@@ -0,0 +1,9 @@
+function ColorMyTerm(color)
+	color = 'gruvbox' or 'green-shades'
+	vim.cmd.colorscheme(color)
+
+	--vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	--vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+
+end
+ColorMyTerm()
